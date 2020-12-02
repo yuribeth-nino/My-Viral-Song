436fb10dd82e0c274bd964d1bc00d78973ad9837
yuribeth-nino <ydcn15@gmail.com>
2020-12-02 10:05:40 +0000

    added models

--- src/app/components/events/songs/songs.component.ts
+++ src/app/components/events/songs/songs.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Songs } from 'src/app/models/songs';
 
 @Component({
   selector: 'app-songs',
   templateUrl: './songs.component.html',
-  styleUrls: ['./songs.component.css']
+  styleUrls: ['./songs.component.css'],
 })
 export class SongsComponent implements OnInit {
+  songs: Songs;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  // call method that is in the interface songs which has the voteup ++
+  voteUp() {
+    this.songs.voteUp();
   }
-
 }
---
+++ src/app/models/Songs.ts
@@ -0,0 +1,37 @@
+// defines the objects that we want to use in the songs component
+
+export class Songs {
+  title: string;
+  artist: string;
+  genre: string;
+  url: string;
+  eventCode: number;
+  votes: number;
+
+  // Constructor class song
+  constructor(
+    title: string,
+    artist: string,
+    genre: string,
+    url: string,
+    votes?: number // the ? means is an optional attribute
+  ) {
+    this.title = title;
+    this.artist = artist;
+    this.genre = genre;
+    this.url = url;
+    this.votes = votes || 0;
+  }
+
+  // <--Methods Class Song-->
+
+  // Method Auto increment number of vote
+  voteUp(): void {
+    this.votes++;
+  }
+
+  // Method to split the url
+  domain(): string {
+    return this.url;
+  }
+}
