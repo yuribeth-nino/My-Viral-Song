f8b94aa34274fde0039638fc8cb09f814caf095c
yuribeth-nino <ydcn15@gmail.com>
2020-12-23 21:52:22 +0000

    adding responsive layout

--- src/app/components/events/songs/new-song/new-song.component.ts
+++ src/app/components/events/songs/new-song/new-song.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Songs } from 'src/app/models/songs';
 import { SongsService } from 'src/app/services/songs.service';
 
@@ -10,22 +11,24 @@ declare var $: any;
   styleUrls: ['./new-song.component.css'],
 })
 export class NewSongComponent implements OnInit {
-  @ViewChild('titleInput', { static: false }) titleInputRef: ElementRef;
-  @ViewChild('artistInput', { static: false }) artistInputRef: ElementRef;
-  @ViewChild('genreInput', { static: false }) genreInputRef: ElementRef;
-  @ViewChild('urlInput', { static: false }) urlInputRef: ElementRef;
+  @ViewChild('f', { static: false }) addSongForm: NgForm;
+  // @ViewChild('titleInput', { static: false }) titleInputRef: ElementRef;
+  // @ViewChild('artistInput', { static: false }) artistInputRef: ElementRef;
+  // @ViewChild('genreInput', { static: false }) genreInputRef: ElementRef;
+  // @ViewChild('urlInput', { static: false }) urlInputRef: ElementRef;
 
   constructor(private songService: SongsService) {}
 
   ngOnInit(): void {}
 
-  onAddSong() {
-    const title = this.titleInputRef.nativeElement.value;
-    const artist = this.artistInputRef.nativeElement.value;
-    const genre = this.genreInputRef.nativeElement.value;
-    const url = this.urlInputRef.nativeElement.value;
-    const newSong = new Songs(title, artist, genre, url);
-    this.songService.addSongs(newSong);
+  onSubmit() {
+    // const title = this.titleInputRef.nativeElement.value;
+    // const artist = this.artistInputRef.nativeElement.value;
+    // const genre = this.genreInputRef.nativeElement.value;
+    // const url = this.urlInputRef.nativeElement.value;
+    // const newSong = new Songs(title, artist, genre, url);
+    // this.songService.addSongs(newSong);
+    console.log(this.addSongForm);
   }
 
   hideModal() {
